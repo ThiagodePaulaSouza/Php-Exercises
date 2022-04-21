3678698dc795fe7217e66286f1ba356943dc16ae
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-04-21 19:17:19 -0300

    input - envia

--- teste/index.php
+++ teste/index.php
@@ -1,22 +1,22 @@
-<?php
-
-function ImprimaItem()
-{
-    $SIM = ["a", "b", "ca"];
-    foreach ($SIM as $item) {
-        echo "$item";
-    }
-}
-?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <h1><?php echo ImprimaItem()?>
+    <h1>
+        <form action="recebe_get.php" method="get">
+            <input type="text" name="nome" id="nome" placeholder="Digite seu nome..."><br/>
+            <input type="text" name="idade" id="idade" placeholder="Digite seu idade..."><br/>
+            <hr><button type="submit">Enviar</button>
+
+        </form>
+    </h1>
 </body>
+
 </html>
